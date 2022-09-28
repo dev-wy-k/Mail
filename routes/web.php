81083380a81ba6784e64ca81d2eec2991090a145
vlain01 <waiyankyw96@gmail.com>
2022-09-28 22:40:31 +0630

    mail verfication & vite setup

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    $postTitle = "Test Mail 1... 2... 3...";
-    $postDescription = "What is Lorem Ipsum?
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
-
-    $sendMails = ['waiyankyw96@gmail.com', 'blamondtuski@gmail.com'];
-
-    foreach($sendMails as $recipient){
-    }
-    Mail::send(new TestingMail($postTitle, $postDescription));
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/mail', function () {
+    $postTitle = 'Test Mail 1... 2... 3...';
+    $postDescription = "What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    Mail::send(new TestingMail($postTitle, $postDescription)) ;
+});
+
+require __DIR__.'/auth.php';
